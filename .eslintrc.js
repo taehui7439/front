144192d353ba746d7c67504b071a7c080ab74720
joshyeom <jhyeom95@daum.net>
2024-04-07 02:08:43 +0900

    Refactor: App.js에 있는 라우터 코드 index.js로 옮김 , Add: README.md, Refactor: .esLintrc.js React.props 타입 스킵 rule 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,12 @@ module.exports = {
   plugins: ["react", "react-hooks", "jsx-a11y"],
   rules: {
     "react/react-in-jsx-scope": "off",
+    "react/prop-types": [
+      "error",
+      {
+        skipUndeclared: true,
+      },
+    ],
   },
   settings: {
     react: { version: "detect" },
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-How weather 리드미
+# git commit convention
 
-국밥
-조쉬
-마고
-매튜
-망고
+Feat : 새로운 기능의 추가
+Fix: 버그 수정
+Docs: 문서 수정 (리드미 작성)
+Style: 스타일 관련 기능(코드 포맷팅, 세미콜론 누락, 코드 자체의 변경이 없는 경우)
+Refactor: 코드 리펙토링
+Test: 테스트 코트, 리펙토링 테스트 코드 추가
+Chore: 빌드 업무 수정, 패키지 매니저 수정(ex .gitignore 수정 같은 경우)
