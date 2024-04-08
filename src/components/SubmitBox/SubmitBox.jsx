8ab63aef1fd322c29e5efc20354163f457b9694b
mi.____.ng <alstjr9438@naver.com>
2024-04-08 16:13:05 +0900

    style: style 수정

--- src/components/SubmitBox/SubmitBox.jsx
+++ src/components/SubmitBox/SubmitBox.jsx
@@ -1,21 +1,23 @@
-import { styled } from "styled-components"
-import { Colors } from "../../tokens/Colors";
+import { styled } from 'styled-components';
+import { Colors } from '../../tokens/Colors';
+import { Fonts } from '../../tokens/Fonts';
 
 export const Button = styled.button`
   cursor: pointer;
-  background-color: ${Colors.line01};
+  background-color: ${Colors.line03};
   border: none;
-  width: 358px;
   height: 59px;
+  margin: 0 20px;
   border-radius: 6px;
   color: white;
   transition: 0.2s ease-in-out;
+  ${Fonts['font-semi-18']};
 
   &:hover {
     background-color: ${Colors.main02};
   }
 `;
 
-export const SubmitBox = ({children, onClick}) => {
-    return <Button onClick={onClick}>{children}</Button>
-}
+export const SubmitBox = ({ children, onClick }) => {
+  return <Button onClick={onClick}>{children}</Button>;
+};
