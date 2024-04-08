6b789c8125f07697992937dced6c28a02125f36f
mi.____.ng <alstjr9438@naver.com>
2024-04-08 17:46:24 +0900

    feat: 인트로 로딩 추가

--- src/pages/intro/index.jsx
+++ src/pages/intro/index.jsx
@@ -16,14 +16,13 @@ const IntroPage = () => {
     age40: false,
   });
   const [body, setBody] = useState({ hot: false, normal: false, cold: false });
-  const [loading, setLoading] = useState(true); // 로딩 상태를 나타내는 상태
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
     }, 3000);
 
-    // 컴포넌트가 unmount 될 때 타이머를 해제합니다.
     return () => clearTimeout(timer);
   }, []);
 
